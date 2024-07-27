3a32af88ef76151cf71a99dc725ffd6430b848a5
Nima Karimipour <karimipour.nima@gmail.com>
2024-07-27 14:19:45 -0700

    annotator script

---
+++ annotator-command.sh
@@ -0,0 +1,7 @@
+lib=true
+arg=true
+poly=true
+cf_version="3.42.0"
+ucrt_version="0.3-SNAPSHOT"
+rm -rvf target > /dev/null
+CF_VERSION=$cf_version UCRT_VERSION=$ucrt_version ANNOTATOR_POLY=$poly ANNOTATOR_LIBRARY=$lib ANNOTATOR_TYPE_ARG=$arg mvn compile
